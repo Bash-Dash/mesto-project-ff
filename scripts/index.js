fcf3bccee0052c39f54cbbe90bedfa8731082bff
Daria Bashilova <Dariaaa.Ky@yandex.ru>
2025-02-19 20:40:16 +0300

    выполнена пр5

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const plasesList = document.querySelector(".places__list");
+
+function createCard(card, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(function (card) {
+  const newCard = createCard(card, deleteCard);
+  plasesList.append(newCard);
+});
